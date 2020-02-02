068b5180e9d040363df367b163acbfa816630218
miguelvd314 <60011305+miguelvd314@users.noreply.github.com>
2020-02-02 13:30:42 -0500

    Add files via upload

---
+++ Algoritmica 2/Laboratorio 1/Estructuta de Control/E1/E2/src/TamañoCadena.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TamañoCadena {
+
+	public static void main(String[] args) {
+		String cadena;
+
+		System.out.println("Escriba una cadena: ");
+		Scanner S = new Scanner(System.in);
+		cadena = S.nextLine();
+		
+		System.out.println(cadena);
+
+	}
+
+}
